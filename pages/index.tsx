fd97427f9268fd3d51c254850e65d4041800d33e
Ezra Singh <singhezra@gmail.com>
2024-01-13 17:26:50 -0500

    Fixed overflow issue in client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { GetStaticProps, InferGetStaticPropsType } from "next";
 import dynamic from "next/dynamic";
 import config from "@/graph.json";
+import "@/app/globals.css";
 
 const Graph = dynamic(
   () => import("@/components/Graph").then((mod) => mod.Graph),
@@ -20,7 +21,7 @@ export default function Home({
   graph,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <main className="overflow-hidden">
+    <main className="max-h-screen max-w-screen">
       <Graph resources={graph} />
     </main>
   );
