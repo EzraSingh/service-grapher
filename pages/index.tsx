1996b12d948812d148d823ed6ebe6b3496066312
Ezra Singh <singhezra@gmail.com>
2024-01-13 16:27:08 -0500

    Develop (#1)
    * Updated CI
    
    * Disabled Git deploy in Vercel for develop branch
    
    * Added Github Actions environment
    
    * Added Vercel Org ID and Project ID to CI env vars
    
    * Using BetaHuhn/deploy-to-vercel-action
    
    * Testing disablign GH deployment
    
    * Testing
    
    * Updated secrets
    
    * Updated rules to prevent multiple deployments
    
    * Updated vercel config
    
    * Disabled github deployment
    
    * Added yarn to deploy job
    
    * Updated deploy GH access token permissions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import type { GetStaticProps, InferGetStaticPropsType } from "next";
 import dynamic from "next/dynamic";
-import process from "process";
-import Jsonnet from "jsonnet";
 
 const Graph = dynamic(
   () => import("@/components/Graph").then((mod) => mod.Graph),
@@ -10,20 +8,15 @@ const Graph = dynamic(
 
 interface StaticProps {
   graph: ResourceType[];
-  result: any;
 }
 
 export const getStaticProps: GetStaticProps<StaticProps> = async () => {
-  const manifestPath = `${process.cwd()}/manifest.jsonnet`;
-  const jsonnet = new Jsonnet();
-  const result = jsonnet.evalFile(manifestPath);
-  console.log({ result });
-  return { props: { graph: [], result } };
+  //const manifestPath = `${process.cwd()}/manifest.jsonnet`;
+  return { props: { graph: [] } };
 };
 
 export default function Home({
   graph,
-  result,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <main className="overflow-hidden">
